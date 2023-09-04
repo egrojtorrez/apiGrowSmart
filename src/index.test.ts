375c726f0e47ac16c320dd9bd5453c460b12d846
egrojtorrez <egrojtorrez@gmail.com>
2023-09-03 17:11:58 -0700

    ✔ Agregando dbname a dokercompose ✔ Cambiando al forma de hacer test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,15 @@
 import { describe, expect, it } from 'vitest'
-import { pool } from './db'
 
-describe('Initial Test', () => {
-  it('BD Work!', async () => {
-    const result = await pool.query('SELECT NOW()')
-    expect(result.rows[0]).toBeDefined()
+describe('Initial Test', async () => {
+  it('Api Work', async () => {
+    const result = await fetch('http://localhost:3000/ping')
+    const status = result.status
+    expect(status).toBe(200)
+  })
+
+  it('BD Work', async () => {
+    const result = await fetch('http://localhost:3000/ping')
+    const data = await result.json()
+    expect(data).toHaveProperty('now')
   })
 })
